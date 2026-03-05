3686173369986b0ec36b43028a8a117932fd3bd2
Hepi34 <105777839+Hepi34@users.noreply.github.com>
2026-03-05 09:22:58 +0100

    Fix start.py

--- pyfiles/start.py
+++ pyfiles/start.py
@@ -1,5 +1,6 @@
 import os
 import sys
+import subprocess
 
 OS = sys.platform
 
@@ -7,11 +8,13 @@ if os.path.exists(".venv"):
     if OS == "win32":
         try:
             os.system(".venv\\Scripts\\activate")
+            print("Virtual environment activated.")
         except Exception as e:
             print(f"Error activating virtual environment: {e}")
     else:
         try:
             os.system("source .venv/bin/activate")
+            print("Virtual environment activated.")
         except Exception as e:
             print(f"Error activating virtual environment: {e}")
 
@@ -20,12 +23,14 @@ else:
         try:
             os.system("python -m venv .venv")
             os.system(".venv\\Scripts\\activate")
+            print("Virtual environment created and activated.")
         except Exception as e:
             print(f"Error setting up virtual environment: {e}")
     else:
         try:
             os.system("python3 -m venv .venv")
             os.system("source .venv/bin/activate")
+            print("Virtual environment created and activated.")
         except Exception as e:
             print(f"Error setting up virtual environment: {e}")
 
@@ -38,20 +43,21 @@ if OS == "win32":
                 os.system(f".venv\\Scripts\\pip install {line.strip()}")
 else:
     with open("requirements.txt", "r") as f:
+        pyver = subprocess.check_output("ls .venv/lib/ | head -n 1", shell=True, text=True).strip()
         for line in f:
-            if os.path.exists(".venv/lib/python3.10/site-packages/" + line.strip()):
+            if os.path.exists(f".venv/lib/{pyver}/site-packages/{line.strip()}"):
                 print(f"Package {line.strip()} is already installed.")
             else:
-                os.system(f"source .venv/bin/pip install {line.strip()}")
+                os.system(f".venv/bin/pip3 install {line.strip()}")
 
 
 if OS == "win32":
     try:
-        os.system("python pyfiles/gui.py")
+        os.system(".venv\\Scripts\\python pyfiles/gui.py")
     except Exception as e:
         print(f"Error running gui.py: {e}")
 else:
     try:
-        os.system("python3 pyfiles/gui.py")
+        os.system(".venv/bin/python3 pyfiles/gui.py")
     except Exception as e:
         print(f"Error running gui.py: {e}")    
